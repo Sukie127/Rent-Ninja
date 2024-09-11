50e39d3f8c9e9c5125459ed8cbe9db42807b6f92
oceansource1 < >
2024-09-11 15:53:32 -0400

    Re-add rental folder

---
+++ code/frontend/rental/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <AppSidebar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    
+  }
+}
+</script>
